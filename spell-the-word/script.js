a81b23ce165a55f1db6f51c49785a3f7b2312a48
kritika-pattalam <kritikarvin2308@gmail.com>
2021-06-09 16:31:31 -0400

    test

--- spell-the-word/script.js
+++ spell-the-word/script.js
@@ -3,22 +3,27 @@ var widget = new Vue({
     data: {
       message: 'Hello Vue!',
       name: '',
+      show: false,
       dictionary : ['apple', 'ball', 'cat', 'dog', 'elephant', 'frog', 'goat', 'hat', 'ice', 'joke', 'kite', 'love', 'monkey', 'name', 'orange', 'price', 'queue', 'right', 'site', 'tank', 'utsav', 'vine', 'white', 'xerox', 'yoyo', 'zebra']
     },
     methods : {
         reset: function() {
-            document.querySelectorAll('.widget-output p').forEach((elem) => elem.remove());
+            this.show = false;
+           // document.querySelectorAll('.widget-output p').forEach((elem) => elem.remove());
             this.name = '';      
         },
         submit: function() {
             let dictionary = this.dictionary;
-            document.querySelectorAll('.widget-output p').forEach((elem) => elem.remove());
+            this.show = false;
+            //document.querySelectorAll('.widget-output p').forEach((elem) => elem.remove());
             let searchTerm = this.name.toLowerCase().split('');
             //let text = searchTerm.toLowerCase().split('');
             
             searchTerm.forEach(function(element) {
                 for(let j=0; j < dictionary.length; j++) { 
                     if (element == dictionary[j].substring(0, 1)) {
+                        this.show = true;
+                        console.log("true");
                         let letter = element.toUpperCase();
                         document.querySelector('.widget-output').innerHTML += `<p>${letter} for ${dictionary[j]}</p>`;
                         break;
